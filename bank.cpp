e41d96ac3121b701a22b923bcb11f9e338f1a156
Sam12-lang <138756911+Sam12-lang@users.noreply.github.com>
2024-04-12 14:25:20 +0530

    Create bank.cpp

---
+++ bank.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class bank
+{
+	private:
+		float deposite,withdrawl,balance=5000;
+		public:
+			bal()
+			{
+				cout<<"enter the balance"<<balance;
+				}
+			withdra()
+			{
+				cout<<"enter amount to withdrawl";
+				cin>>withdrawl;
+				balance=withdrawl-balance;
+				cout<<"your balance is";
+				cin>>balance:
+				}
+			deposite()
+			{
+				cout"<<enter the amount to deposite":
+				cin>>deposite;
+				
